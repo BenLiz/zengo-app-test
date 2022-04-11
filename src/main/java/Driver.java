64c6d6a23992073ef6e11258feed2522b790599e
Ben Lizmi <benlizmi@macbook-pro-2.mynet>
2022-04-11 15:04:31 +0300

    commit

---
+++ src/main/java/Driver.java
@@ -0,0 +1,44 @@
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+        String testResults;
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "78ac3949");
+        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        cap.setCapability("appPackage", "com.zengo.wallet");
+        cap.setCapability("appActivity", "com.zengo.MainActivity");
+        cap.setCapability("unlockType", "pattern");
+        cap.setCapability("unlockKey", "12369");
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        driver.findElement(By.xpath(Elements.homeBtn)).click();
+        driver.findElement(By.xpath(Elements.assetMngr)).click();
+        driver.findElement(By.xpath(Elements.bitcoinBtn)).click();
+        driver.findElement(new AppiumBy.ByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Dogecoin\"));"));
+        driver.findElement(By.xpath(Elements.dogecoinBtn)).click();
+        driver.findElement(By.xpath(Elements.closeMngr)).click();
+
+        if (driver.findElement(By.xpath(Elements.dogecoinTxt)).isDisplayed() && !driver.findElement(By.xpath(Elements.bitcoinBtn)).isDisplayed()) {
+            testResults = "Test Passed";
+
+        }
+        else
+        {
+            testResults = "Test Failed";
+
+
+        }
+        System.out.println(testResults);
+    }
+}
---
+++ src/main/java/Elements.java
@@ -0,0 +1,10 @@
+public class Elements {
+
+    static String homeBtn = "//*[@class='android.view.View' and ./*[@text='Home'] and ./*[@class='android.view.ViewGroup']]";
+    static String assetMngr = "//*[@contentDescription='wallet-screen-button-asset-manager']";
+    static String bitcoinBtn = "//*[@class='android.view.ViewGroup' and ./parent::*[@class='android.view.ViewGroup'] and (./preceding-sibling::* | ./following-sibling::*)[@text='Bitcoin'] and (./preceding-sibling::* | ./following-sibling::*)[@class='android.widget.ImageView'] and ./*[@class='android.view.ViewGroup' and ./*[@class='android.view.ViewGroup' and ./*[@class='android.view.ViewGroup']]]]";
+    static String dogecoinBtn = "//*[@class='android.view.ViewGroup' and ./parent::*[@class='android.view.ViewGroup'] and (./preceding-sibling::* | ./following-sibling::*)[@text='Dogecoin'] and (./preceding-sibling::* | ./following-sibling::*)[@class='android.widget.ImageView'] and ./*[@class='android.view.ViewGroup' and ./*[@class='android.view.ViewGroup' and ./*[@class='android.view.ViewGroup']]]]";
+    static String closeMngr = "//*[@class='android.view.ViewGroup' and ./*[@class='android.widget.ImageView'] and (./preceding-sibling::* | ./following-sibling::*)[@text='Asset Manager'] and (./preceding-sibling::* | ./following-sibling::*)[./*[@class='android.view.ViewGroup']]]";
+    static String dogecoinTxt ="//*[@text='Dogecoin']";
+    static String bitcoincoinTxt ="//*[@text='Bitcoin']";
+}
